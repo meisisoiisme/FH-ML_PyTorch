677fb8c90cac3e1bdb82142ccbf7c440acae38ed
meisisoiisme <64416712+meisisoiisme@users.noreply.github.com>
2024-01-21 21:24:19 +0100

    Update AlexNet_Derivate_2.py

--- CNN/AlexNet/AlexNet_Derivate_2.py
+++ CNN/AlexNet/AlexNet_Derivate_2.py
@@ -2,7 +2,7 @@ class Derivate_2_AlexNet(nn.Module):
     def __init__(self, num_classes=6):
         super(Derivate_2_AlexNet, self).__init__()
         self.features = nn.Sequential(
-            nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3),  # Adjusted kernel and stride
+            nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3),     # Adjusted kernel and stride
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=3, stride=2),
             nn.Conv2d(64, 128, kernel_size=5, padding=2),
@@ -14,9 +14,9 @@ class Derivate_2_AlexNet(nn.Module):
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=3, stride=2),
         )
-        self.avgpool = nn.AdaptiveAvgPool2d((5, 5))  # Adjusted output size
+        self.avgpool = nn.AdaptiveAvgPool2d((5, 5))                   # Adjusted output size
         self.classifier = nn.Sequential(
-            nn.Dropout(0.4),  # Adjusted dropout rate
+            nn.Dropout(0.4),                                          # Adjusted dropout rate
             nn.Linear(256 * 5 * 5, 1024),
             nn.ReLU(),
             nn.Linear(1024, num_classes),
