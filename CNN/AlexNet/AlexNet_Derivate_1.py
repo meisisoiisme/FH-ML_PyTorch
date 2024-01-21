0c3e383e302a4a5ab9ec73cd4198fd9bbcccba2f
meisisoiisme <64416712+meisisoiisme@users.noreply.github.com>
2024-01-21 20:28:02 +0100

    Create AlexNet_Derivate_1.py

---
+++ CNN/AlexNet/AlexNet_Derivate_1.py
@@ -0,0 +1,30 @@
+class Derivate_1_AlexNet(nn.Module):
+    def __init__(self, num_classes=6):
+        super(DifferentAlexNet, self).__init__()
+        self.features = nn.Sequential(
+            nn.Conv2d(3, 32, kernel_size=7, stride=2, padding=3),      # Decreased initial channels
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+            nn.Conv2d(32, 64, kernel_size=5, padding=2),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+            nn.Conv2d(64, 128, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(128, 128, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+        )
+        self.avgpool = nn.AdaptiveAvgPool2d((4, 4))                    # Adjusted output size
+        self.classifier = nn.Sequential(
+            nn.Dropout(0.3),                                           # Adjusted dropout rate
+            nn.Linear(128 * 4 * 4, 512),                               # Adjusted output features
+            nn.ReLU(),
+            nn.Linear(512, num_classes),
+        )
+
+    def forward(self, x):
+        x = self.features(x)
+        x = self.avgpool(x)
+        x = torch.flatten(x, 1)
+        x = self.classifier(x)
+        return x
